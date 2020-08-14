45483b74ab226ddc7391b09c26eb0ef563410b23
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-14 19:45:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-
-
+var billAmount=document.getElementById("billAmount");
+var tipPercent=document.getElementById("tipPercent");
+var numPeople=document.getElementById("numPeople");
 
 function calculate(){
 

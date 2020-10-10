1348bc5383f00a308d164935454c0ef207f9c966
Leslie Wen <leslie.wen@gmail.com>
2020-10-10 13:20:45 -0400

    0.0.02

--- main.js
+++ main.js
@@ -3,21 +3,16 @@ var tipPercent=document.getElementById("tipPercent");
 var numPeople=document.getElementById("numPeople");
 var textContainer=document.getElementById("text-container");
 
-var output;
-
-
-//Need to do error handling
-
 function calculate(){
 	output=(billAmount.value*(tipPercent.value/100))/numPeople.value
 
-	textContainer.innerHTML="Tip: $"+output
+	textContainer.innerHTML="You should tip: $"+output
 	textContainer.classList.remove("hide-me");
 }
 
 document.getElementById("submit-button").onclick = function() {
 	if(billAmount.value<=0){
-		alert("You cannot have a bill amount of zero");
+		alert("You cannot have a bill amount of zero or less");
 	}else if(tipPercent.value<0){
 		alert("You cannot have a negative tip percentage rate")
 	}else{
